e48603eb0b1b5c4f5725cbe694309aaf18f7e399
blakcmeOW <martinbattulayan@gmail.com>
2023-10-03 22:39:00 +0800

    Update code for Challenge 2.

--- pokedex.py
+++ pokedex.py
@@ -3,12 +3,13 @@ import sys
 
 def search_pokemon(name):
   #response = requests.get(f"https://pokeapi.co/api/v2/pokemon/{name}/")
-  pokemon_name = str(input("Your favorite pokemon: "))
-  response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(pokemon_name))
+  #pokemon_name = str(input("Your favorite pokemon: "))
+  #response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(pokemon_name))
+  response = requests.get("https://pokeapi.co/api/v2/pokemon/{}".format(name))
   pokemon = response.json()
   print("Name: " + str.capitalize(pokemon["name"]))
   print("ID: " + str(pokemon["id"]))
-  #print("BaseXP: " + str(pokemon["levels"][0][1]))
+  print("BaseXP: " + str(pokemon["base_experience"]))
   
 if __name__ == "__main__":
   search_pokemon(sys.argv[1])
@@ -18,7 +19,7 @@ if __name__ == "__main__":
 #ID: <pokemon ID>
 #Base XP: <pokemon baseXP>
 
-#Output1
+#Output1 Must store 6 pokemons and show information about them
 #Name: <pokemon name>
 #HP: <pokemon HP>
 #Attacks: <pokemon attacks>
